472f5b6cb07c63ca2792cc0f45abb3224f604073
James Reed <james@twiddlingbits.net>
2022-06-23 17:30:38 -0600

    Redesign inline code style

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,26 +6,18 @@ module.exports = {
       screens: {
         "md-960": "960px",
       },
-      colors: {
-        "inline-code-fg": "hsl(355, 65%, 65%)",
-        "inline-code-bg": "hsla(220, 14%, 71%, 0.15)",
-      },
-      typography: (theme) => ({
+      typography: () => ({
         DEFAULT: {
           css: {
             code: {
-              backgroundColor: theme("colors.inline-code-bg"),
-              color: theme("colors.inline-code-fg"),
-              fontWeight: "400",
-              "border-radius": "0.25rem",
+              color: "hsl(5, 48%, 51%)",
+              fontWeight: "bold",
             },
             "code::before": {
               content: '""',
-              "padding-left": "0.25rem",
             },
             "code::after": {
               content: '""',
-              "padding-right": "0.25rem",
             },
           },
         },
