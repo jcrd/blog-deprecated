608f3033f0e5740386f011e1b5c67c9964b833ea
James Reed <james@twiddlingbits.net>
2022-08-02 15:22:01 -0400

    Remove `url` filter and pathPrefix definition

--- .eleventy.js
+++ .eleventy.js
@@ -43,18 +43,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/static": "." })
   eleventyConfig.addWatchTarget("./src/styles/")
 
-  var pathPrefix = ""
-  if (process.env.GITHUB_REPOSITORY) {
-    pathPrefix = process.env.GITHUB_REPOSITORY.split("/")[1]
-  }
-
   return {
     dir: {
       input: "src",
       output: "public",
     },
     markdownTemplateEngine: "njk",
-    pathPrefix,
   }
 }
 
